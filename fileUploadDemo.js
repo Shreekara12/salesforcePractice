ba06589dcde134cdd5a3e1353d3f442d9907232c
Shreekara12 <ps.shreekara@gmail.com>
2025-01-29 23:47:29 +0530

    Add files via upload

---
+++ fileUploadDemo.js
@@ -0,0 +1,27 @@
+import { LightningElement, api } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent'
+
+export default class FileUploadDemo extends LightningElement {
+    @api myRecordId
+
+    get acceptedFormats(){
+        return ['.png', '.pdf']
+    } 
+
+    handleFileUpload(event){
+        const uploadedFiles = event.detail.files;
+        let uploadedFileNames = '';
+        for(let i=0; i<uploadedFiles.length; i++){
+        uploadedFileNames += uploadedFiles[i].name + ', ';
+        }
+        this.dispatchEvent(
+        new ShowToastEvent({
+        title:'Success',
+        message:uploadedFiles.length + ' files uploaded successfully: ' + uploadedFileNames,
+        variant: 'success',
+        }),
+        );
+    }
+        
+
+}
